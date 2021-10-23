645eb499bb4b08b3bfd3504abd25a879ac4b8ac5
AndrewMishchenko <AndrewMishchenko001@gmail.com>
2021-10-23 16:48:43 +0300

    finally

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Section from "./Section";
+import ImageFinder from "./components/ImageFinder";
+
+export default function App() {
+  return (
+    <Section>
+      <ImageFinder />
+    </Section>
+  );
+}
